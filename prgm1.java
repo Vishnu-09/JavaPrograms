e12d7b1257e8e67d3a82af31d239a7c37f04254f
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2022-11-12 23:06:37 +0530

    new

--- prgm1.java
+++ prgm1.java
@@ -12,3 +12,17 @@ System.out.println("Hello, Java.");
     }
 }
 Next program
+// hello world program in C
+
+
+
+ // hello world program in JAVA 
+
+public class Solution {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
+Next program
