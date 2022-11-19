d033ecb92f2e9578115213150091c0b6e872857a
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2022-11-19 17:40:45 +0530

    new

--- Quadratic.java
+++ Quadratic.java
@@ -21,3 +21,21 @@ public class Quadratic {
 			System.out.format("root1=%.2f and root2=%.2f",root1,root2);
 		}
 	
+		}
+		else if(d==0)
+		{
+			root1=root2=-b/(2*a);
+			System.out.println("the roots are real and equal");
+			System.out.format("root1=root2=%.2f:",root1);
+		}
+		else
+		{
+			double real = -b/(2*a);
+			double imaginary=Math.sqrt(-d)/(2*a);
+			System.out.println("the roots are imaginary");
+			System.out.format("root1=%.2f+%.2f;,",real,imaginary);
+			System.out.format("root1=%.2f-%.2f;,",real,imaginary);
+		}
+	}
+
+}
