60e74cb6a23f2dfbfc739cf18d9a1f50c7794b0e
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2022-12-19 00:03:24 +0530

    new

---
+++ Switch.java
@@ -0,0 +1,40 @@
+package com.basicvishnu;
+import java.util.*;
+
+class Switch 
+{
+	
+public static void main(String[] args) {
+		
+Scanner ip=new Scanner(System.in);
+System.out.println("enter your operator +  - * /");
+char operator=ip.next().charAt(0);
+System.out.println("enter your first command");
+double first=ip.nextInt();
+System.out.println("enter your second command");
+double second=ip.nextInt();
+double result=0;
+switch(operator)
+{
+	case '+':
+	result=first+second;
+  System.out.println("the result is:"+first+operator+second+"="+result );
+  break;
+	case '-':
+	result=first-second;
+	  System.out.println("the result is:" +first+operator+second+"="+result);
+	  break;
+	case '*':
+	result=first*second;
+	  System.out.println("the result is:" +first+operator+second+"="+result);
+	  break;
+	case '/':
+	result=first/second;
+	  System.out.println("the result is:" +first+operator+second+"="+result);
+	  break;
+	  default :
+		  System.out.println("invalid operator");
+		  break;
+}
+}
+}
