e375537f8bb1b576dc2e8645224c70201354ae24
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2023-01-10 23:57:54 +0530

    new

--- Quadratic.java
+++ Quadratic.java
@@ -1,19 +1,4 @@
-package com.basicvishnu;
-import java.util.Scanner;
-public class Quadratic {
 
-	public static void main(String[] args) {
-
-		int a,b,c;
-		double root1,root2;
-		System.out.println("Enter the coefficients");
-		Scanner in=new Scanner(System.in);
-		a = in.nextInt();
-		b = in.nextInt();
-		c = in.nextInt();
-		double d=b*b-4*a*c;
-		System.out.println("determinant="+d);
-		if(d>0)
 		{
 			root1=(-b+Math.sqrt(d))/(2*a);
 			root2=(-b+Math.sqrt(d))/(2*a);
